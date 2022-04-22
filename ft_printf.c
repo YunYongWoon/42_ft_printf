c6207545eb3c7ca18f45493cbe56c619c0aeee31
Yongwoon Yun <yoyun@c3r1s4.42seoul.kr>
2022-04-22 17:03:05 +0900

    fix branch

---
+++ ft_printf.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoyun <yoyun@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:08:24 by yoyun             #+#    #+#             */
+/*   Updated: 2022/04/22 16:50:25 by yoyun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	ap;
+	int		ret_val;
+
+	va_start(ap, s);
+	ret_val = 0;
+	while (*s)
+	{
+		if (*s == '%')
+		{
+			if (*s == '%')
+				ret_val += write(1, *s, 1);
+			else
+			{	
+				ret_val += logic_funtion(ap, *s);
+				s++;
+			}
+		}
+		else
+			ret_val += write(1, *s, 1);
+		s++;
+	}
+	va_end(ap);
+	return (ret_val);
+}
